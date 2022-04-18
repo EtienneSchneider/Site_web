43ff83634ad2489670fce585d9a70b333e322dbc
Etienne Schneider <79159796+EtienneSchneider@users.noreply.github.com>
2022-04-18 11:20:59 +0200

    blablabla

--- site/script.js
+++ site/script.js
@@ -1,10 +1,5 @@
-window.addEventListener("DOMContentLoaded", function(){
-   
+window.addEventListener("DOMContentLoaded", function(){  
     var navbar = document.getElementsByClassName("navbar")[0];
-  
-   
-  
-
     window.addEventListener('scroll', function(e) {
         if(this.window.scrollY > 5){
             navbar.classList.add("scrollNavbar");
@@ -16,5 +11,27 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     
     });
+
+
+    let menu = document.getElementById("burg-menu")
+    let burger = document.getElementById("burgerlogo")
+    let cross = document.getElementById("crosslogo")
+
+    burger.addEventListener('click', function(){
+        menu.classList.add("burg-active");
+        menu.style.display = "block";
+        burger.style.display = "none";
+        cross.style.display = "block";
+    });
+
+    
+    cross.addEventListener('click', function(){
+        menu.classList.remove("burg-active");
+        menu.style.display = "none";
+        burger.style.display = "block";
+        cross.style.display = "none";
+    });
+
+
 });
 
